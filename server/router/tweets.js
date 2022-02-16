7c6257f0a74cb52c7b837d80607ea2c77128aec7
lkhhh <fsit1534@naver.com>
2022-02-16 22:52:38 +0900

    dwitter server

---
+++ server/router/tweets.js
@@ -0,0 +1,27 @@
+import express from 'express';
+
+const tweets=[
+    {
+        id='1',
+        text:'드림코딩',
+        createAt:Date.now().toString(),
+        name:'Bob',
+        username:'bob',
+        url:'',
+    }
+]
+const router=express.Router();
+
+//GET/tweets
+//GET/tweets?username=username
+router.get('/',(req,res,next)=>{
+    const username=req.query.username;
+    const data=username ? tweets.filter(t=>t.username===username)
+    :tweets;
+    res.status(200).json(data);
+});
+//GET/tweets/id
+//POST/tweets
+//PUT/tweets/:id
+
+export default router;
